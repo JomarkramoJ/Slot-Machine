8693327285449f3886ffda96927752180e98ea63
Jomark <maguadjomarkglenn@gmail.com>
2024-10-01 22:37:22 +0800

    Slot_Machine

--- SlotMachine.py
+++ SlotMachine.py
@@ -15,5 +15,26 @@ def main():
     print("  Symbols: 🍇 🍊 🥝 🔔 💵")
     print("******************************")
 
+    while balance > 0:
+        print(f"Current balance: 💲{balance}")
+
+        bet = input("Place your bet amount: ")
+
+        if not bet.isdigit():
+            print("Please enter a valid number")
+            continue
+
+        bet = int(bet)
+
+        if bet > balance:
+            print("Insufficient Balance")
+            continue
+        if bet <= 0:
+            print("Bet must be grater than 0")
+
+        balance -= bet
+
+        row = spin_row()
+
 if __name__ == '__main__':
     main()
